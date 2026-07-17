c1ee1b200850777f859e56908d14c69502dfcab9
sayed <abdelghanytarek0@gmail.com>
2026-07-17 15:05:57 +0300

    prevent Resend from throwing in CI build

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,3 +1,3 @@
 import { Resend } from "resend";
 import { env } from "./env";
-export const resend = new Resend(env.RESEND_API_KEY);
+export const resend = new Resend(env.RESEND_API_KEY || "re_build_placeholder");
